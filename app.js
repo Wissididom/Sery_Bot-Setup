001e81329ff18daac25f2b9136959c39380bd811
Wissididom <30803034+Wissididom@users.noreply.github.com>
2022-11-24 12:24:38 +0100

    Moved Modding to Helix API

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var accessToken = getHashValue('access_token');
-var username = null;
+var user = null;
 var client;
 function getHashValue(key) {
 	matches = location.hash.match(new RegExp(key+'=([^&]*)'));
@@ -12,7 +12,7 @@ async function connectAndStartSetup() {
 			username,
 			password: accessToken
 		},
-		channels: [username, 'sery_bot']
+		channels: [user.login, 'sery_bot']
 	});
 	await client.connect().then(([server, port]) => {
 		var statusBox = document.getElementById('statusBox');
@@ -25,13 +25,60 @@ async function connectAndStartSetup() {
 		statusBox.classList = 'red';
 		console.error(`Error connecting to Twitch Chat: ${err}`);
 	});
-	if ((await client.mods(username)).includes('sery_bot')) {
-		document.getElementById('modSery').innerText = 'Made sure Sery_Bot a Mod ✅';
+	makeSureSeryBotIsMod();
+}
+async function makeSureSeryBotIsMod() {
+	let mods = (await fetch(`https://api.twitch.tv/helix/moderation/moderators?broadcaster_id=${user.id}&user_id=402337290`, { // 402337290 = Sery_Bot
+		headers: {
+			'Client-ID': '3mae1yavxj9shdb5ucwpn3w7s54lie',
+			'Authorization': `Bearer ${accessToken}`
+		}
+	}).then(res => res.json()).then(json => json.data.map(d => d.user_id)));
+	if (mods.includes('402337290')) { // 402337290 = Sery_Bot
+		document.getElementById('modSery').innerText = 'Made sure Sery_Bot a Mod ✅ (Sery_Bot already is a mod)';
 		document.getElementById('seryAuthorized').style.display = 'inline';
 	} else {
-		client.mod(username, 'sery_bot').then(() => {
-			document.getElementById('modSery').innerText = 'Made sure Sery_Bot a Mod ✅';
-			document.getElementById('seryAuthorized').style.display = 'inline';
+		await fetch(`https://api.twitch.tv/helix/moderation/moderators?broadcaster_id=${user.id}&user_id=402337290`, { // 402337290 = Sery_Bot
+			method: 'POST',
+			headers: {
+				'Client-ID': '3mae1yavxj9shdb5ucwpn3w7s54lie',
+				'Authorization': `Bearer ${accessToken}`
+			}
+		}).then(res => {
+			switch (res.status) {
+				case 204: // 204 No Content
+					document.getElementById('modSery').innerText = 'Made sure Sery_Bot a Mod ✅ (Modded Sery_Bot)';
+					document.getElementById('seryAuthorized').style.display = 'inline';
+					break;
+				case 400: // 400 Bad Request - Must be banned because everything else cannot happen (broadcaster_id not found, user_id not found, already a mod)
+					document.getElementById('modSery').innerText = 'Couldn\'t make sure Sery_Bot a Mod ❌: Twitch returned 400 Bad Request, probably Sery_Bot was banned.';
+					document.getElementById('authorizeSeryBtn').style.display = 'none';
+					break;
+				case 401: // 401 Unauthorized
+					document.getElementById('modSery').innerText = 'Couldn\'t make sure Sery_Bot a Mod ❌: Twitch returned 401 Unauthorized, probably the access token misses the channel:manage:moderators scope or is not valid anymore. Try reauthorizing!';
+					document.getElementById('authorizeSeryBtn').style.display = 'none';
+					break;
+				case 422: // 422 Unprocessable Entity - User is a VIP
+					let vipRes = await fetch(`https://api.twitch.tv/helix/moderation/moderators?broadcaster_id=${user.id}&user_id=402337290`, { // 402337290 = Sery_Bot
+						method: 'DELETE',
+						headers: {
+							'Client-ID': '3mae1yavxj9shdb5ucwpn3w7s54lie',
+							'Authorization': `Bearer ${accessToken}`
+						}
+					});
+					if (vipRes.status == 204) {
+						document.getElementById('modSery').innerText = 'Removed Sery_Bot\' VIP Status for it to be able to become a Mod...';
+						document.getElementById('authorizeSeryBtn').style.display = 'none';
+					} else {
+						document.getElementById('modSery').innerText = 'Couldn\'t make sure Sery_Bot a Mod ❌: Removing VIP Status for it to become a Mod failed! Try removing VIP manually and then use the script again!';
+						document.getElementById('authorizeSeryBtn').style.display = 'none';
+					}
+					break;
+				case 429: // 429 Too Many Requests
+					document.getElementById('modSery').innerText = 'Couldn\'t make sure Sery_Bot a Mod ❌: Twitch returned 429 Too Many Requests, You\'ve probably added 10 moderators withhin 10 seconds. How is this even possible';
+					document.getElementById('authorizeSeryBtn').style.display = 'none';
+					break;
+			}
 		}).catch(err => {
 			document.getElementById('modSery').innerText = `Couldn't make sure Sery_Bot a Mod ❌: ${err}`;
 			document.getElementById('authorizeSeryBtn').style.display = 'none';
@@ -122,12 +169,12 @@ if (window.accessToken) {
 	document.getElementById('authorizeSetupBtn').style.display = 'none';
 	document.getElementById('authorizeSeryBtn').style.display = 'inline';
 	(async () => {
-		window.username = (await fetch('https://api.twitch.tv/helix/users', {
+		window.user = (await fetch('https://api.twitch.tv/helix/users', {
 			headers: {
 				'Client-ID': '3mae1yavxj9shdb5ucwpn3w7s54lie',
 				'Authorization': `Bearer ${accessToken}`
 			}
-		}).then(res => res.json()).then(json => json.data[0].login));
+		}).then(res => res.json()).then(json => json.data[0]));
 		connectAndStartSetup();
 	})();
 }
